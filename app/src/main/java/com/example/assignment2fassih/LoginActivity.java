fc36f525a62421156f9511ad833783f312809a97
MuhammadFassihHaider <fassih.haider786@gmail.com>
2019-10-29 15:09:28 +0500

    Set error messages to fields in Login page

--- app/src/main/java/com/example/assignment2fassih/LoginActivity.java
+++ app/src/main/java/com/example/assignment2fassih/LoginActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,16 +13,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     public static final String MY_PREFS = "myPrefs";
     public static final String USER = "myUser";
     public static final String PASS = "myPass";
-    private EditText mUsername, mPassword;
+    private EditText mEmail, mPassword;
     private Button mButton;
     private TextView mRegister;
     private Spinner mChoiceUser;
-    private String users[] = {"User", "Doctor", "Guest"};
+    private String[] users = {"User", "Doctor", "Guest"};
     private String masterPassword, masterEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +47,11 @@ public class LoginActivity extends AppCompatActivity implements AdapterView.OnIt
         masterEmail = getIntent().getStringExtra("email");
         masterPassword = getIntent().getStringExtra("password");
 
-        mUsername = (EditText) findViewById(R.id.editText);
-        mPassword = (EditText) findViewById(R.id.editText2);
-        mButton = (Button) findViewById(R.id.button);
-        mRegister = (TextView) findViewById(R.id.lnkRegister);
-        mChoiceUser = (Spinner) findViewById(R.id.choiceSpinner);
+        mEmail = findViewById(R.id.editText);
+        mPassword = findViewById(R.id.editText2);
+        mButton = findViewById(R.id.button);
+        mRegister = findViewById(R.id.lnkRegister);
+        mChoiceUser = findViewById(R.id.choiceSpinner);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -68,7 +71,7 @@ public class LoginActivity extends AppCompatActivity implements AdapterView.OnIt
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity2();
+                dataValidation();
             }
         });
     }
@@ -83,15 +86,42 @@ public class LoginActivity extends AppCompatActivity implements AdapterView.OnIt
         // TODO - Custom Code
     }
 
-    public void openActivity2(){
+    public void openHomeActivity() {
 
-        if(mUsername.getText().toString().equals(masterEmail) && mPassword.getText().toString().equals(masterPassword)){
+        if (mEmail.getText().toString().equals(masterEmail) && mPassword.getText().toString().equals(masterPassword)) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        } else if (mEmail.getText().toString().equals("fassih") && mPassword.getText().toString().equals("123")) {
             Intent intent = new Intent(this, HomeActivity.class);
             startActivity(intent);
         }
-        else if(mUsername.getText().toString().equals("fassih") && mPassword.getText().toString().equals("123")){
-            Intent intent = new Intent(this, HomeActivity.class);
-            startActivity(intent);
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    private void dataValidation() {
+        if (isEmpty(mEmail)) {
+            Toast t = Toast.makeText(this, "You must enter a valid email to login!", Toast.LENGTH_SHORT);
+            t.show();
+            mEmail.setError("Email is required!");
+        } else if (isEmail(mEmail) == false) {
+            Toast t = Toast.makeText(this, "Enter a valid email to login!", Toast.LENGTH_SHORT);
+            t.show();
+            mEmail.setError("Valid email is required!");
+        } else if (isEmpty(mPassword)) {
+            Toast t = Toast.makeText(this, "You must enter your password to login!", Toast.LENGTH_SHORT);
+            t.show();
+            mPassword.setError("Password is required!");
+        } else {
+            openHomeActivity();
         }
     }
 
